c6d61b920d2b957c9409aaada703f7d7040626b4
VladislavHub <darkdevil9592@gmail.com>
2023-08-10 12:43:11 +0300

    lesson done

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function calculateExpression(expression) {
   //Преревіряємо чи аргумент є рядком
   //Якщо так повертаємо виконананий код
   // Якщо ні повертаємо текст "Argument is not a string"
+  if (typeof expression === 'string') {
+    return eval(expression)
+  } else {
+    return "Argument is not a string"
+  }
 }
 
 console.log("Завдання 1 ====================================");
@@ -21,6 +26,14 @@ function getNumberFromString(s) {
   // Перевіряємо, чи є отримане значення числом за допомогою isNaN
   // Якщо значення не є числом, повертаємо текст "Couldn't extract a number from the provided string."
   // Якщо значення є числом, повертаємо його
+
+  let num = parseFloat(s)
+
+  if (isNaN(num)) {
+    return "Couldn't extract a number from the provided string."
+  } else {
+    return num
+  }
 }
 
 // Виводимо результати виклику функції
@@ -32,7 +45,8 @@ console.log(
 console.log(
   'getNumberFromString("No numbers here")',
   getNumberFromString("No numbers here")
-); // Виведе повідомлення про помилку
+);
+ // Виведе повідомлення про помилку
 
 // Задача 3: Напишіть функцію, яка приймає рядок та повертає ціле число з цього рядка або повідомлення про помилку.
 
@@ -42,6 +56,14 @@ function getIntegerFromString(s) {
   // Перевіряємо, чи є отримане значення числом за допомогою isNaN
   // Якщо значення не є числом, повертаємо текст "Couldn't extract an integer from the provided string."
   // Якщо значення є числом, повертаємо його
+  
+  let num = parseInt(s)
+
+  if (isNaN(num)) {
+    return "Couldn't extract an integer from the provided string."
+  } else {
+    return num
+  }
 }
 
 // Виводимо результати виклику функції
@@ -53,7 +75,8 @@ console.log(
 console.log(
   'getIntegerFromString("No integers here")',
   getIntegerFromString("No integers here")
-); // Виведе повідомлення про помилку
+); 
+// Виведе повідомлення про помилку
 
 // Задача 4: Напишіть функцію, яка перевіряє, чи є введене число скінченним.
 
@@ -64,6 +87,12 @@ function isNumberFinite(num) {
   // За допомогою оператора if перевіряємо, чи є число скінченним.
   // Якщо число є скінченним, повертаємо текст "The number is finite.".
   // Якщо число не є скінченним, повертаємо текст "The number is not finite.".
+  
+  if (isFinite(num)) {
+    return "The number is finite."
+  } else {
+    return "The number is not finite."
+  }
 }
 
 // Виконуємо функцію з різними вхідними даними і виводимо результат.
@@ -80,6 +109,11 @@ function isValueNaN(value) {
   // За допомогою оператора if перевіряємо, чи є значення NaN.
   // Якщо значення є NaN, повертаємо текст "The value is NaN.".
   // Якщо значення не є NaN, повертаємо текст  "The value is not NaN.".
+  if (isNaN(value)) {
+    return "The value is NaN."
+  } else {
+    return "The value is not NaN."
+  }
 }
 
 // Виконуємо функцію з різними вхідними даними і виводимо результат.
@@ -93,6 +127,9 @@ console.log("isValueNaN(100)", isValueNaN(100)); // Виведе "The value is n
 function createURLObject(urlStr) {
   // Використовуємо вбудований конструктор URL, щоб створити новий об'єкт URL з введеного рядка.
   // Повертаємо створений об'єкт URL.
+  
+  let url = new URL(urlStr)
+  return url
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -100,7 +137,8 @@ console.log("Завдання 6 ====================================");
 console.log(
   'createURLObject("https://example.com")',
   createURLObject("https://example.com")
-); // Виведе URL об'єкт
+); 
+// Виведе URL об'єкт
 
 // Задача 7: Напишіть функцію, яка кодує компонент URL.
 
@@ -109,6 +147,8 @@ function encodeURLComponent(urlComponent) {
   // Використовуємо вбудовану функцію encodeURIComponent, щоб закодувати введений компонент URL.
   // Ця функція повертає закодований компонент URL, замінивши небезпечні символи на їх процентне кодування.
   // Повертаємо закодований компонент URL.
+
+  return encodeURIComponent(urlComponent)
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -116,7 +156,8 @@ console.log("Завдання 7 ====================================");
 console.log(
   'encodeURLComponent("Hello World!")',
   encodeURLComponent("Hello World!")
-); // Виведе "Hello%20World!"
+); 
+// Виведе "Hello%20World!"
 
 // Задача 8: Напишіть функцію, яка кодує URL.
 
@@ -125,6 +166,8 @@ function encodeURL(url) {
   // Використовуємо вбудовану функцію encodeURI, щоб закодувати введений URL.
   // Ця функція повертає закодований URL, замінивши небезпечні символи на їх процентне кодування.
   // Повертаємо закодований URL.
+
+  return encodeURI(url)
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -132,7 +175,8 @@ console.log("Завдання 8 ====================================");
 console.log(
   'encodeURL("https://ex ample.com")',
   encodeURL("https://ex ample.com")
-); // Виведе "https://ex%20ample.com"
+); 
+// Виведе "https://ex%20ample.com"
 
 // Задача 9: Напишіть функцію, яка декодує закодований компонент URL.
 
@@ -141,6 +185,8 @@ function decodeURLComponent(urlComponent) {
   // Використовуємо вбудовану функцію decodeURIComponent, щоб декодувати введений закодований компонент URL.
   // Ця функція повертає декодований компонент URL, замінивши процентне кодування символів на їх реальні значення.
   // Повертаємо декодований компонент URL.
+
+  return decodeURIComponent(urlComponent)
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -148,7 +194,8 @@ console.log("Завдання 9 ====================================");
 console.log(
   'decodeURLComponent("Hello%20World%21")',
   decodeURLComponent("Hello%20World%21")
-); // Виведе "Hello World!"
+); 
+// Виведе "Hello World!"
 
 // Задача 10: Напишіть функцію, яка декодує закодований URL.
 
@@ -157,6 +204,8 @@ function decodeURL(url) {
   // Використовуємо вбудовану функцію decodeURI, щоб декодувати введений закодований URL.
   // Ця функція повертає декодований URL, замінивши процентне кодування символів на їх реальні значення.
   // Повертаємо декодований URL.
+
+  return decodeURI(url)
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -164,4 +213,5 @@ console.log("Завдання 10 ====================================");
 console.log(
   'decodeURL("https://ex%20ample.com")',
   decodeURL("https://ex%20ample.com")
-); // Виведе "https://ex ample.com"
+); 
+// Виведе "https://ex ample.com"
